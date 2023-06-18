4d0c0807e2c1de9fe03ffd604777cd9e52542008
tungika91 <40808780+tungika91@users.noreply.github.com>
2023-06-18 10:11:42 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Bus from './Bus';
+import Nav from './Nav';
 
 function App() {
+  const URL = "http://localhost:3500/Services";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Bus Arrival from SWTC to Joo Koon MRT</h1>
+      <Nav />
+      <Bus URL = { URL }/>
+    </>
   );
 }
 
---
+++ src/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <>
+      <input placeholder='253 or 258' type='text'></input>
+      <button>Select</button>
+    </>
+  )
+}
