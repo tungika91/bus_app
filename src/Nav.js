c9dff4db8c8292d5ef039172f1b3ae1674d42cb1
tungika91 <40808780+tungika91@users.noreply.github.com>
2023-06-23 21:00:18 +0800

    update time display

--- src/Nav.js
+++ src/Nav.js
@@ -3,13 +3,15 @@ import React from 'react'
 export default function Nav({ currentDate, busStopCode }) {
   const date = `${currentDate.getDate()}`;
   const month = currentDate.toLocaleString('default', {month: 'long'});
-  const time = currentDate.getHours() + ':' + currentDate.getMinutes();
+  const hour = currentDate.getHours()
+  const minute = currentDate.getMinutes();
+  
   
   return (
     <div className='Header'>
       <div className='topright'>
           <h1>Bus Stop: {busStopCode} </h1>
-          <h2> { time } <br/> { date } { month }</h2>
+          <h2> { hour }:{minute<10?'0':''}{minute} <br/> { date } { month }</h2>
       </div>
     </div>
   )
