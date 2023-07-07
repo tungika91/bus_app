2098acd5914060319dc3446992731330c52a2ee1
tungika91 <40808780+tungika91@users.noreply.github.com>
2023-07-07 19:50:30 +0800

    add Golf bookmark

--- src/Bookmark.js
+++ src/Bookmark.js
@@ -9,10 +9,15 @@ export default function Bookmark( { setBusStopCode } ) {
         setBusStopCode('23021');
     }
 
+    function select945() {
+        setBusStopCode('43641');
+    }
+
     return (
         <>
             <button className='select' onClick = { select258 } >258</button>
             <button className='select' onClick = { select253 } >253</button>
+            <button className='select' onClick = { select945 } >Golf</button>
         </>
     )
 }
