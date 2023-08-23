4467143c133575d6e8e7961a7709b16ba738b064
tungika91 <40808780+tungika91@users.noreply.github.com>
2023-08-23 19:38:07 +0800

    add refresh button

--- src/Nav.js
+++ src/Nav.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { IoReloadCircle } from 'react-icons/io5';
 
 export default function Nav({ currentDate, busStopCode }) {
   const date = `${currentDate.getDate()}`;
@@ -6,11 +7,14 @@ export default function Nav({ currentDate, busStopCode }) {
   const hour = currentDate.getHours()
   const minute = currentDate.getMinutes();
   
-  
+  function handleRefresh() {
+    window.location.reload();
+  }
   return (
     <div className='Header'>
       <div className='topright'>
           <h1>Bus Stop: {busStopCode} </h1>
+          <button onClick = { handleRefresh }><IoReloadCircle/></button>
           <h2> {hour<10 ? '0' : ''}{ hour }:{minute < 10 ? '0' : ''}{ minute } <br/> { date } { month.substring(0,3) }</h2>
       </div>
     </div>
